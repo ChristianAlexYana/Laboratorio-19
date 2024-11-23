4e5f50bacd067d1a5f7caf69d0ed98745777018d
Gustavo <glinares@unsa.edu.pe>
2024-11-23 16:17:38 -0500

    Clase Ejercito creada

--- Laboratorio-19/src/laboratorio/pkg19/Ejercito.java
+++ Laboratorio-19/src/laboratorio/pkg19/Ejercito.java
@@ -4,10 +4,40 @@
  */
 package laboratorio.pkg19;
 
-/**
- *
- * @author Gustavo
- */
+import java.util.ArrayList;
+import java.util.Random;
 public class Ejercito {
     
+    String nombre;
+    ArrayList<Soldado> soldados;
+    public Ejercito(String nombre) {
+        this.nombre = nombre;
+        this.soldados = new ArrayList<>();
+        generarSoldados();
+    }
+    private void generarSoldados() {
+        Random random = new Random();
+        int cantidadSoldados = random.nextInt(10) + 1;
+        for (int i = 0; i < cantidadSoldados; i++) {
+            int tipoSoldado = random.nextInt(3);
+            Soldado soldado;
+            switch (tipoSoldado) {
+                case 0 : soldado = new Espadachin(nombre + ":E" + (i + 1));break;
+                case 1 :  soldado = new Arquero(nombre + ":A" + (i + 1));break;
+                default : soldado = new Caballero(nombre + ":C" + (i + 1));break;
+            }
+            soldados.add(soldado);
+        }
+    }
+    public ArrayList<Soldado> getSoldados() {
+        return soldados;
+    }
+    public double promedioVida() {
+        return soldados.stream().mapToInt(Soldado::getVida).average().orElse(0);
+    }
+
+    public Soldado getSoldadoMayorVida() {
+        return soldados.stream().max((s1, s2) -> Integer.compare(s1.getVida(), s2.getVida())).orElse(null);
+    }
 }
+
