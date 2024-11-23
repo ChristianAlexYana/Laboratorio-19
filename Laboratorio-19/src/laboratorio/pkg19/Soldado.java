61022fe5c3bef52a9bd5a579679f327abb4f8704
Gustavo <glinares@unsa.edu.pe>
2024-11-23 16:58:52 -0500

    Clase Soldado mejorada 02

--- Laboratorio-19/src/laboratorio/pkg19/Soldado.java
+++ Laboratorio-19/src/laboratorio/pkg19/Soldado.java
@@ -5,18 +5,37 @@
 package laboratorio.pkg19;
 
 public class Soldado {
-    String nombre;
-    int vida;
+    private String nombre;
+    private int vida;
+    private int posX;
+    private int posY;
+
     public Soldado(String nombre, int vida) {
         this.nombre = nombre;
         this.vida = vida;
     }
+
     public int getVida() {
         return vida;
     }
+
     public String getNombre() {
-        return nombre;       
+        return nombre;
     }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosicion(int x, int y) {
+        this.posX = x;
+        this.posY = y;
+    }
+
     public void incrementarVida() {
         this.vida += 1;
     }
